5e98052bbb965f4eebf62dce0b7d545c2874b041
Joe Harris <joeharris76@gmail.com>
2026-09-16 17:40:04 -0400

    chore: start v0.1.5 development cycle

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/).
 
+## [Unreleased]
+
 ## [0.1.4] - 2026-09-16
 
 ### Fixed
--- src/textcharts/__init__.py
+++ src/textcharts/__init__.py
@@ -23,7 +23,7 @@ Basic usage::
     print(chart.render())
 """
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 # ---------------------------------------------------------------------------
 # Base classes and configuration
