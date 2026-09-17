81e6b368c714ac3f2b1b8faeef06a60bf05e0856
Joe Harris <joeharris76@gmail.com>
2026-09-17 08:12:46 -0400

    chore: start v0.1.6 development cycle

--- src/textcharts/__init__.py
+++ src/textcharts/__init__.py
@@ -23,7 +23,7 @@ Basic usage::
     print(chart.render())
 """
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 # ---------------------------------------------------------------------------
 # Base classes and configuration
